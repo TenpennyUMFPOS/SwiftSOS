5a7b76b70ceffccfd31842981af5302e374260c1
TenpennyUMFPOS <med19aziz@gmail.com>
2024-04-20 09:33:28 +0100

    fixing middleware problem in order to deploy

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,18 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default authMiddleware({
-    publicRoutes: ['/']
+
+
+const isProtectedRoute = createRouteMatcher([
+    '/dashboard(.*)',
+    '/forum(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+    if (isProtectedRoute(req)) auth().protect();
 });
-
 export const config = {
-    matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: [
+        '/((?!.*\\..*|_next).*)', // Don't run middleware on static files
+        '/', // Run middleware on index page
+        '/(api|trpc)(.*)'], // Run middleware on API routes
 };
