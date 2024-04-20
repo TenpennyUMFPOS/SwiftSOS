b0f3f445f091a20240c38933f31c03d86d14fb8b
TenpennyUMFPOS <med19aziz@gmail.com>
2024-04-20 07:46:48 +0100

    finished authentication with Clerk

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,3 +18,49 @@ const config: Config = {
   plugins: [],
 };
 export default config;
+const plugin = require('tailwindcss/plugin');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    screens: {
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    colors: {
+      black: '#000',
+      white: '#fff',
+      orange: '#E06330',
+      gray: '#E0D7C8',
+      brown: '#996630'
+    },
+    fontFamily: {
+      // Headings
+      sans: ['Inter', 'sans-serif'],
+      // Base text
+      monospace: ['Inconsolata', 'monospace'],
+    },
+    fontSize: {
+      xs: '.75rem',
+      sm: '.875rem',
+      tiny: '.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+    },
+    letterSpacing: {
+      wide: '.025em',
+      wider: '.05rem',
+      widest: '.1rem'
+    },
+  },
+};
