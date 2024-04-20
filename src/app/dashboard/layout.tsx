b491f337d027bf6a9737e2a1662369640d2f4629
TenpennyUMFPOS <med19aziz@gmail.com>
2024-04-20 14:55:56 +0100

    test on profile complete in layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,16 @@
 import { RedirectType, redirect } from "next/navigation";
 import Aside from "../_components/aside";
+import { auth } from "@clerk/nextjs/server";
+import isUserReady from "../actions/isUserReady";
 
 export default async function DashboardLayout({
     children, // will be a page or nested layout
 }: {
     children: React.ReactNode;
 }) {
+    const { userId } = auth();
+    const userReady = await isUserReady();
+    if (!userReady) redirect("/complete-profile", RedirectType.replace);
     return (
         <div className="grid h-screen w-full pl-[56px]">
             <Aside />
